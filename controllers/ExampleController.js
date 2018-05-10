60de51af134a151fdc03d599be694fee9f8a5d64
David Nee Yartey <yarteyd@gmail.com>
2018-05-10 12:14:27 +0000

    Added an example controller.

---
+++ controllers/ExampleController.js
@@ -0,0 +1,9 @@
+// const bcrypt = require('bcrypt');
+// const response = require('../responses');
+
+/**
+ * All functions are accessible to
+ */
+exports.hello = function (req, res) {
+    res.send(response.success([],'Your example controller.'));
+}
