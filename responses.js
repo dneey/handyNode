aca59126486e39bbcb087d79f3015432b17faf49
David Nee Yartey <yarteyd@gmail.com>
2018-05-10 12:13:11 +0000

    response formatted

--- responses.js
+++ responses.js
@@ -1,3 +1,13 @@
+/**
+ * This is where you can set your responses.
+ * Useful if you intend to use a response at many places.
+ * 
+ * No need to re create it, call this class and pass it your response methods.
+ * Thats it!
+ * 
+ * You can add as many as you like.
+ */
+
 exports.success = function (data = [], message = 'Operation Successful', status='200') {
 
     return response( data, message, status );
@@ -12,9 +22,9 @@ exports.invalidCredentials = function (data = [], message = 'Invalid Credentials
 function response(data = [], message = '', status = '200'){
     // Check if its an array else make it an array
     console.log('data' + ' ' + data);
-    if (typeof(data) != []) {
-        data = [data];
-    }
-    return { 'data': data, 'responseMessage': message, 'responseCode': status };
+    // if (typeof(data) != []) {
+    //     data = [data];
+    // }
+    return { 'responseMessage': message, 'responseCode': status ,'data': data };
 }
 // exports.response = response();
