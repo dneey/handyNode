aff198e60d3e793cc198df9875fa8d44be78e517
David Nee Yartey <yarteyd@gmail.com>
2018-05-10 12:13:45 +0000

    added controller register to register all controllers created

---
+++ controllers/Controller.js
@@ -0,0 +1,10 @@
+/**
+ * This is where you register all your controllers,
+ *
+ * Controllers registered here can be accessed in the api.js
+ * 
+ * After registering, you can access it in your api.js
+ */
+
+exports.ExampleController = require('../controllers/ExampleController'); 
+exports.AuthController = require('../controllers/AuthController'); 
