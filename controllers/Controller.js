fb12ee99d12bcc018f30aede9b47992fc6692a3c
David Nee Yartey <yarteyd@gmail.com>
2018-05-10 12:48:55 +0000

    comments

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,9 +1,11 @@
 /**
+ * ------------------------------------------------------
  * This is where you register all your controllers,
- *
+ 
  * Controllers registered here can be accessed in the api.js
- * 
+ 
  * After registering, you can access it in your api.js
+ * --------------------------------------------------------
  */
 
 exports.ExampleController = require('../controllers/ExampleController'); 
