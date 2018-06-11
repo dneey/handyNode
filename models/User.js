d486cdb008bd85696361eb453b71e1a9b921214c
David Nee Yartey <yarteyd@gmail.com>
2018-06-11 12:35:30 +0000

    user model for sequelize

---
+++ models/User.js
@@ -0,0 +1,9 @@
+// const db = require('../config/db');
+
+// const User = db.connection.define('user',{
+//     name: db.Sequelize.STRING,
+//     email: db.Sequelize.STRING,
+//     password: db.Sequelize.STRING
+// });
+// exports.connection = db.connection;
+// connection.sync();
