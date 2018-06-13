cd7507dadf8cf0fc2611101e06b79fe762aa36f8
David Nee Yartey <yarteyd@gmail.com>
2018-06-13 16:32:53 +0000

    firebase confg

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var firebase = require("firebase");
+        var config = {
+        apiKey: "AIzaSyCwtsJU-pX7cpnNhYzdJ_4q1qSmqvVAsRI",
+        authDomain: "texttext-6728b.firebaseapp.com",
+        databaseURL: "https://texttext-6728b.firebaseio.com",
+        projectId: "texttext-6728b",
+        storageBucket: "texttext-6728b.appspot.com",
+        messagingSenderId: "986312653658"
+      };
+      firebase.initializeApp(config);
